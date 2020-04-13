472dcc5d2142d52a5501d6f106f77bd1b7033c58
Augusto Pereira <augusto.pereira@mgmcorp.com.br>
2020-04-13 14:46:56 -0300

    criando chamado frontend

--- Callhub.Presentation.Frontend/callhub/src/components/Calls/New/StepConfirmation/index.js
+++ Callhub.Presentation.Frontend/callhub/src/components/Calls/New/StepConfirmation/index.js
@@ -1,9 +1,34 @@
 import React from "react";
 import { connect } from "react-redux";
+import Api from "../../../../services/Api";
 import FileList from "../../../Upload/FileList";
 import "./style.css";
 
-const StepConfirmation = ({ call, setStep }) => {
+const StepConfirmation = ({ call, setStep, accessToken }) => {
+  const handleConfirmation = async () => {
+
+    const data = {
+      title: call.title,
+      description: call.description,
+      priority: call.priority.id,
+      sectorDestinId: "30f6593e-ed42-4a4d-8588-b74bfaa61a62",
+    };
+
+    console.log("Upping request with data", data);
+
+    try {
+      const response = await Api.post("/calls", data, {
+        headers: {
+          Authorization: `Bearer ${accessToken}`
+        }
+      });
+
+      console.warn("Api response", response);
+    } catch (error) {
+      console.error(error.message);
+    }
+  };
+
   return (
     <div className="ContentConfirmation">
       <h3 className="PageTitle">Confirmação</h3>
@@ -30,9 +55,7 @@ const StepConfirmation = ({ call, setStep }) => {
         </a>
 
         <button
-          onClick={() => {
-            /* STORE CALL */
-          }}
+          onClick={handleConfirmation}
         >
           Confirmar
         </button>
@@ -43,6 +66,7 @@ const StepConfirmation = ({ call, setStep }) => {
 
 const mapStateToProps = (state) => ({
   call: state.call.newCall,
+  accessToken: state.user.accessToken
 });
 
 export default connect(mapStateToProps)(StepConfirmation);
